e519231f9d0ea5af20f9cc23c7b4826dee465c30
Dheisom <dheisomgomes0@gmail.com>
2022-11-06 08:53:51 -0300

    Update routes management

--- api/routes.py
+++ api/routes.py
@@ -1,10 +1,10 @@
 from . import extractors
 from sanic import Sanic, Request, json
-
+from http.server import HTTPStatus
 
 def add_all(server: Sanic):
     server.add_route(home, '/', methods=["GET"])
-    server.add_route(search, '/search/<query>', methods=["GET"])
+    server.add_route(search, '/search', methods=["GET"])
     server.add_route(download, '/download/<name>', methods=["GET"])
 
 
@@ -12,33 +12,52 @@ async def home(_):
     "Obtem todos os filmes/series recomendados pelo site"
     try:
         recommendations = await extractors.home.get_all()
-    except Exception as error:
-        result, code = {'error': f'failed to get homepage recommendations: {error}'}, 500
-    else:
-        result, code = {'results': list(map(lambda r: r.dict(), recommendations))}, 200
-    return json(result, code)
+    except Exception as e:
+        return json(
+            body={'error': f'failed to get homepage recommendations: {str(e)}'},
+            status=HTTPStatus.INTERNAL_SERVER_ERROR
+        )
+    return json(
+        body=list(map(lambda r: r.dict(), recommendations)),
+        status=HTTPStatus.OK
+    )
 
 
-async def search(request: Request, query: str):
+async def search(request: Request):
     "Procura pelo filme desejado no site"
+    query = request.args.get('q')
+    if not query:
+        return json(
+            body={'error': 'query not specified'},
+            status=HTTPStatus.BAD_REQUEST
+        )
     page = int(request.args.get("page", "1"))
     try:
         search_results, has_next = await extractors.search.search(query, page)
-    except Exception as error:
-        result, code = {'error': f'failed to search: {error}'}, 500
-    else:
-        result = {
+    except Exception as e:
+        return json(
+            body={'error': f'failed to search: {str(e)}'},
+            status=HTTPStatus.INTERNAL_SERVER_ERROR
+        )
+    return json(
+        body={
             'page': {
                 'number': page,
                 'has_next': has_next
             },
             'results': list(map(lambda r: r.dict(), search_results))
-        }
-        code = 200
-    return json(result, code)
+        },
+        status=HTTPStatus.OK
+    )
 
 
 async def download(_, name: str):
     "Obtem as informações de download de um filme/serie"
-    result = await extractors.download.get_download(name)
-    return json(result.dict())
+    try:
+        result = await extractors.download.get_download(name)
+    except Exception as e:
+        return json(
+            body={'error': f'failed to get download informations: {str(e)}'},
+            status=HTTPStatus.INTERNAL_SERVER_ERROR
+        )
+    return json(body=result.dict(), status=HTTPStatus.OK)
