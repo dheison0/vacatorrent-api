232e9084e4f399157b2da0042c67fa600b3373c3
Dheisom <dheisomgomes0@gmail.com>
2022-11-20 10:46:50 -0300

    code improvedments

--- api/extractors/download.py
+++ api/extractors/download.py
@@ -12,19 +12,19 @@ async def get_download(location: str) -> Download:
     title = root.find('h1', class_='t_pagina').text
     title = title.split('TORRENT')[-1].split('DOWNLOAD')[0].strip()
     imdb = root.find('div', class_='col-sm-5').find('a', itemprop='sameAs')
-    imdb = (0.0 if not imdb else float(imdb.text))
     return Download(
         title=title.title(),
         sinopse=sinopse.text.replace(sinopse.next.text, ''),
         thumbnail=root.find('img', class_='img-responsive capa_imagem').get('src'),
-        imdb=imdb,
+        imdb=(0.0 if not imdb else float(imdb.text)),
         links=extract_links(root)
     )
 
 
 def extract_links(root: BeautifulSoup) -> list[Link]:
-    links = []
     tag_attrs = {'class': 'list-group-item list-group-item-success newdawn'}
-    for tag in root.findAll('a', tag_attrs):
-        links += [Link(title=tag.get('title'), url=tag.get('href'))]
+    links = list(map(
+        lambda i: Link(i.get('title'), i.get('href')),
+        root.findAll('a', tag_attrs)
+    ))
     return links
