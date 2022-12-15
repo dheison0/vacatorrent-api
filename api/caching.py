848113028d5fa7865a35836122acf4756fce165b
Dheisom <dheisomgomes0@gmail.com>
2022-12-15 17:51:34 -0300

    Improve cache mechanism

--- api/caching.py
+++ api/caching.py
@@ -1,26 +1,45 @@
-from typing import Any
-from time import sleep
-from threading import Thread
+from dataclasses import dataclass
 from functools import wraps
+from random import randint
+from threading import Thread
+from time import sleep, time
 import gc
 
 
-def cache_response(timeout: int = 60) -> Any:
-    def decorator(func):
-        response: Any = None
+@dataclass
+class Cache:
+    expire: float
+    data: any
 
-        def clean():
-            nonlocal response
-            sleep(timeout)
-            response = None
-            gc.collect()
+
+STORAGE: dict[int, Cache] = {}
+MANAGERS: int = 0
+
+
+def cache_manager():
+    """Manage cache storage"""
+    global  MANAGERS
+    MANAGERS += 1
+    while len(STORAGE) > 0 and MANAGERS == 1:
+        sleep(1)
+        for cid, cache in list(STORAGE.items()):
+            if time() >= cache.expire:
+                STORAGE.pop(cid)
+    MANAGERS -= 1
+    gc.collect()
+
+
+def cache_response(expire: int = 60):
+    def decorator(func):
+        cache_id = randint(1e10, 1e11-1)
 
         @wraps(func)
         async def wrapper(*args, **kwargs):
-            nonlocal response
-            if not response:
+            if cache_id not in STORAGE:
                 response = await func(*args, **kwargs)
-                Thread(target=clean).start()
-            return response
+                STORAGE[cache_id] = Cache(time()+expire, response)
+                if len(STORAGE) == 1:
+                    Thread(target=cache_manager).start()
+            return STORAGE[cache_id].data
         return wrapper
     return decorator
