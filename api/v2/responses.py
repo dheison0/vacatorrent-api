9c84e24a605e749395c5677e0a740e6869b7960c
Dheisom Gomes <dheisomgomes0@gmail.com>
2023-01-02 16:08:45 -0300

    Rename response.py to responses.py and add more types

---
+++ api/v2/responses.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Recommendation:
+    title: str
+    genre: str
+    thumbnail: str
+    year: int
+    rating: float
+    url: str
+    id: str
+
+
+@dataclass
+class Link:
+    title: str
+    url: str
+
+
+@dataclass
+class Download:
+    title: str
+    sinopse: str
+    rating: float | None
+    thumbnail: str
+    links: list[Link]
+
+
+@dataclass
+class SearchResult:
+    title: str
+    sinopse: str
+    thumbnail: str
+    url: str
+    path: str
