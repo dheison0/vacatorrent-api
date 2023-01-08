755c016614303123c961b6f1469c117e1450de13
Dheisom Gomes <dheisomgomes0@gmail.com>
2023-01-08 09:30:38 -0300

    Fix pytest search.getResults test error

--- api/v2/extractors/search_test.py
+++ api/v2/extractors/search_test.py
@@ -7,10 +7,11 @@ from .search import getResults
 
 @pytest.mark.asyncio
 async def test_getResults():
-    resultPageOne = await getResults('alice', 1)
+    resultPageOne, hasMore = await getResults('alice', 1)
     assert isinstance(resultPageOne, list)
     assert len(resultPageOne) > 0
     assert isinstance(resultPageOne[0], SearchResult)
+    assert hasMore == True
     resultPageTwo = await getResults('alice', 2)
     assert resultPageOne != resultPageTwo
 
